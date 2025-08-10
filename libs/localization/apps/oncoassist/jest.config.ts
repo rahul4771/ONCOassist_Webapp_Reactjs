e93506a12cda13867173340b28246ff0db3810a4
rahulv-pmt <rahul@portablemedicaltechnology.com>
2025-08-10 17:54:11 +0530

    Updated with local setup issue fixes

--- libs/localization/apps/oncoassist/jest.config.ts
+++ libs/localization/apps/oncoassist/jest.config.ts
@@ -1,6 +1,6 @@
 export default {
   displayName: 'oncoassist',
-  preset: '../../jest.preset.js',
+  preset: '../../../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
